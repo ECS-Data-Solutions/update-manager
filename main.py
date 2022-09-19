03c71e32373148cfaa24f17b2e51975834e16504
Boxersteavee <astroscope101@gmail.com>
2022-09-19 14:46:15 +0100

    Adding code to (hopefully) make it work

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import websockets
-
-
-
+import asyncio
+import json
+import os
+    async def update_server():
+        async with websockets.connect("ws://update.ecsdatasolutions.xyz") as websocket:
+            message = await websocket.recv()
+            if json.loads(message) == {"msg": "update"}:
+                os.system("docker-compose down")
+                os.system("docker-compose up-d")
